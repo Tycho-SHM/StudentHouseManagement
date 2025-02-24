5f5e623013e99492d73cfc6814408114d09ac4a3
Tycho van Isterdael <tycho@vanisterdael.net>
2025-02-24 12:50:35 +0100

    Cleanup API Project

---
+++ NotificationService/NotificationService.API/Controllers/SettingsController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NotificationService.API.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class SettingsController : ControllerBase
+{
+    private readonly ILogger<SettingsController> _logger;
+
+    public SettingsController(ILogger<SettingsController> logger)
+    {
+        _logger = logger;
+    }
+    
+    [HttpGet(Name = "GetSettings")]
+    public IEnumerable<string> Get()
+    {
+        _logger.LogTrace("SettingsController::GetSettings called");
+        return ["value1", "value2"];
+    }
+}
