3dd11d32ab974750b256e2b20b08ebf71a4431cc
Tycho van Isterdael <tycho@vanisterdael.net>
2025-02-27 12:59:05 +0100

    Removed MessageQueue projects from repo since they have moved to their own repo using nuget packages

--- NotificationService/SHM.MessageQueues.Abstractions/IMessageBrokerConnection.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace SHM.MessageQueues.Abstractions;
-
-public interface IMessageBrokerConnection
-{
-    public Task PublishMessageToQueue(string queueName, string message);
-    public Task SubscribeToQueue(string queueName, Func<string, Task> messageHandler);
-}
