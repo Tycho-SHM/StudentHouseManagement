9cafca5ac5b88c2730c181f4ef1d997feb4d61e0
Tycho van Isterdael <tycho@vanisterdael.net>
2025-03-18 22:33:54 +0100

    Added basic clerk based authentication

---
+++ Frontend/src/lib/components/SignIn.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+    import { onMount } from 'svelte';
+    import { getClerk } from '$lib/clerk';
+
+    let signInDiv: any;
+    let clerk;
+
+    onMount(async () => {
+        clerk = await getClerk();
+        if (signInDiv && !clerk.user) {
+            clerk.mountSignIn(signInDiv);
+        }
+    });
+</script>
+
+<div bind:this={signInDiv} class="clerk-sign-in-container"></div>
---
+++ Frontend/src/lib/components/SignOut.svelte
@@ -0,0 +1,14 @@
+<script>
+    import { getClerk } from '$lib/clerk';
+
+    async function handleSignOut() {
+        const clerk = await getClerk();
+        await clerk.signOut();
+        // Optionally redirect after sign out
+        window.location.href = '/';
+    }
+</script>
+
+<button on:click={handleSignOut} class="sign-out-button">
+    Sign Out
+</button>
