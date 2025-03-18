dabc607f67e98a182113c0708f79f4ef8a85a4f7
Tycho van Isterdael <tycho@vanisterdael.net>
2025-03-18 20:57:50 +0100

    Recreate frontend

--- Frontend/src/routes/+page.svelte
+++ Frontend/src/routes/+page.svelte
@@ -1,8 +1,2 @@
-<script>
-    import MessagePoster from "$lib/MessagePoster.svelte";
-</script>
-
-<h1>Student House Management</h1>
-<p>Using this project you will be able to plan cleaning and other tasks in your student house.</p>
-<a href="settings">Settings</a>
-<MessagePoster />
+<h1>Welcome to SvelteKit!</h1>
+<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
