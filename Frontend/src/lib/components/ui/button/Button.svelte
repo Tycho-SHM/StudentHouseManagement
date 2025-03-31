198518b75cfadb68379ff1a3dd344c4cb0cb10fe
Tycho van Isterdael <tycho@vanisterdael.net>
2025-03-31 13:39:52 +0200

    Friendship with SvelteKit ended. VueJs is my new best friend

--- Frontend/src/lib/components/ui/button/Button.svelte
+++
@@ -1,8 +0,0 @@
-<script lang="ts">
-    let { children, ...restProps } = $props();
-</script>
-
-<button {...restProps}
-    class="bg-amber-500 hover:bg-amber-600 text-white font-bold py-2 px-4 rounded hover:cursor-pointer">
-    {@render children()}
-</button>
---
+++ Frontend/src/views/HomeView.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+  import IconSupport from "@/components/icons/IconSupport.vue";
+</script>
+
+<template>
+  <main>
+    This is the homeview
+    <IconSupport />
+  </main>
+</template>
