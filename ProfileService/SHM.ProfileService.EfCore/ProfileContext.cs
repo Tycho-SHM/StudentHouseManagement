fc123e9ed09e9cfe67942cd0179647335c550530
Tycho van Isterdael <tycho@vanisterdael.net>
2025-05-16 00:46:14 +0200

    Switch to EfCore

---
+++ ProfileService/SHM.ProfileService.EfCore/ProfileContext.cs
@@ -0,0 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using SHM.ProfileService.Model;
+using SHM.ProfileService.Model.House;
+using SHM.ProfileService.Model.Invite;
+
+namespace SHM.ProfileService.EfCore;
+
+public class ProfileContext : DbContext
+{
+    public DbSet<UserProfile> UserProfiles { get; set; }
+    public DbSet<HouseProfile> HouseProfiles { get; set; }
+    public DbSet<HouseMembership> HouseMemberships { get; set; }
+    public DbSet<Invite> Invites { get; set; }
+}
